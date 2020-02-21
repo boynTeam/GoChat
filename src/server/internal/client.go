47f740ee50308b007d9c414137c4074f9b6b74a8
KaserChan <chankaser@gmail.com>
2020-02-21 13:02:51 +0800

    feat(server.client): 为客户端增加新字段
    增加了客户端的标识码与状态.
    其中状态有未登录,已登录,已退出,未注册,注册功能暂时没有,但是为以后留出位置

--- src/server/internal/client.go
+++ src/server/internal/client.go
@@ -17,15 +17,25 @@ const clientChannelBufferSize = 10
 
 type Client struct {
 	Ip       string      //客户端的ip
+	Number   string      //客户端的标识码
 	Name     string      //客户端在登录的时候指定的名i在
 	Channel  chan string //用于收发客户端的消息
 	Conn     net.Conn    // 客户端的TCP连接
+	State    int         // 客户端状态
 	LastSend time.Time
 }
+
+const (
+	NotLoggedIn = iota // 未登录
+	LoggedIn           // 已经登录
+	Exited             // 已经退出
+	Registering        // 未注册
+)
 
 func NewClient(name string, conn net.Conn) (cli *Client) {
 	return &Client{
 		Ip:       conn.RemoteAddr().String(),
+		Number:   conn.RemoteAddr().String(),
 		Name:     name,
 		Channel:  make(chan string, clientChannelBufferSize),
 		Conn:     conn,
