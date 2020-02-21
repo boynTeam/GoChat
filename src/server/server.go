12c6ba0c5a7bd83a7254bb1f5e71d2176df6012d
KaserChan <chankaser@gmail.com>
2020-02-21 12:20:41 +0800

    feat(server): 在退出时清理资源
    在服务端关闭时,将未关闭的客户端关闭

--- src/server/server.go
+++ src/server/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	. "server/internal"
 )
 
 /**
@@ -15,6 +16,7 @@ date:2020/2/15
 主函数,用于打开TCP端口监听以及处理广播和连接
 */
 func Serve(port int) {
+	defer Stop()
 	listener, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
 	if err != nil {
 		log.Fatal(err)
@@ -29,4 +31,13 @@ func Serve(port int) {
 		}
 		go handleConn(conn)
 	}
+}
+
+// 在退出的时候清理资源
+func Stop() {
+	// 将所有已经连接的客户端关闭
+	Clients.Range(func(key, value interface{}) bool {
+		_ = value.(Client).Conn.Close()
+		return true
+	})
 }
