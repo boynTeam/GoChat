4e8b0f016e5cbcf5cd4fbaa79531fb204a699583
KaserChan <chankaser@gmail.com>
2020-02-21 11:51:32 +0800

    refactor(client.protocol): 解耦协议的读写部分
    protocol.read部分负责读取网络中传来的消息,并将其解析为协议的不同部分.在实际运行中,read部分用一个协程运行,并使用通道将数据传出
    protocol.write部分负责将消息打包传入服务器中

---
+++ src/client/protocol/read.go
@@ -0,0 +1,45 @@
+package protocol
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"net"
+)
+
+// Author:Boyn
+// Date:2020/2/21
+
+func packetSlitFunc(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	// 检查 atEOF 参数 和 数据包头部的四个字节是否 为 0x123456(我们定义的协议的魔数)
+	if !atEOF && len(data) > 6 && binary.BigEndian.Uint32(data[:4]) == 0xABC123 {
+		var l uint16
+		// 读出 数据包中 实际数据 的长度(大小为 0 ~ 2^32)
+		binary.Read(bytes.NewReader(data[4:6]), binary.BigEndian, &l)
+		pl := int(l) + 6
+		if pl <= len(data) {
+			return pl, data[:pl], nil
+		}
+	}
+	return
+}
+
+func ResolveMessage(conn net.Conn, output chan string) {
+	var buf [65542]byte
+	result := bytes.NewBuffer(nil)
+	for {
+		n, err := conn.Read(buf[0:])
+		result.Write(buf[0:n])
+		if err != nil && err != io.EOF {
+			fmt.Println("传输数据错误:", err)
+			break
+		}
+		scanner := bufio.NewScanner(result)
+		scanner.Split(packetSlitFunc)
+		for scanner.Scan() {
+			output <- scanner.Text()[6:]
+		}
+	}
+}
